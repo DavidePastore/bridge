921bba682db829c1018d5ca11f86e3a518bfd327
alfsb <alfsb@users.noreply.github.com>
2017-08-29 00:28:05 -0300

    Update svnprops.php

--- src/svnprops.php
+++ src/svnprops.php
@@ -1,76 +1,49 @@
 <?php
 
-require_once dirname( __FILE__ ) . '/SvnUtil.php';
-
-if ( count ( $argv ) < 3 )
+if ( count ( $argv ) < 2 )
 {
-	print "Use: " . $argv[0] . " diff_path repo_dir [strip]\n";
+	print "Use: " . $argv[0] . " path\n";
 	return -1;
 }
 
-SvnUtil::$Verbose = true;
+$path = $argv[1];
 
-$diff = $argv[1];
-$repo = $argv[2];
-$strp = isset( $argv[3] ) ? (int) $argv[3] : 0;
+$cmd = '/usr/bin/svn proplist -R ' . escapeshellarg( $path );
+$out = shell_exec( $cmd );
+$lines = explode( "\n" , $out );
 
-$tmpfile = getcwd() . "/diff.tmp";
+$filename = "";
+$props = [];
 
-$stat = SvnUtil::Status( $repo );
-if ( count( $stat->Files ) > 0 )
+foreach ( $lines as $line )
 {
-	print "Repositório local modificado, impossível continuar (status com " . count( $stat->Files ) . " registros).\n\n";
-	die( 1 );
+    if ( strlen( $line ) == 0 ) continue;
+    
+    if ( $line[0] != ' ' )
+    {
+        CheckProps( $filename , $props );
+        
+        $pos = strpos( $line , "'" );
+        $filename = substr( $line , $pos + 1 );
+        $filename = substr( $filename , 0, strlen( $filename ) -1 );
+        $props = [];
+    }
+    else
+    {
+        $props[] = trim( $line );
+    }
 }
 
-print "Download $diff\n";
+function CheckProps( $filename , $props )
 {
-	$curl = curl_init();
-	curl_setopt( $curl , CURLOPT_AUTOREFERER , TRUE );
-	curl_setopt( $curl , CURLOPT_HEADER , 0 );
-	curl_setopt( $curl , CURLOPT_RETURNTRANSFER , 1 ) ;
-	curl_setopt( $curl , CURLOPT_URL , $diff );
-	curl_setopt( $curl , CURLOPT_FOLLOWLOCATION , TRUE );
-	$contents = curl_exec( $curl );
-	curl_close( $curl );
-	file_put_contents( $tmpfile , $contents );
+    if ( substr( $filename , -4 ) != '.xml' )
+        return;
+    
+    //echo "# $filename\n";
+    
+    if ( in_array( "svn:eol-style" , $props ) == false )
+        echo "svn propset svn:eol-style 'native' $filename\n";
+    
+    if ( in_array( "svn:keywords" , $props ) == false )
+        echo "svn propset svn:keywords 'Revision' $filename\n";
 }
-
-print "Patch\n";
-{
-	$old = getcwd();
-	chdir( $repo );
-
-	$cmd = "/usr/bin/svn patch --strip " . $strp . " " . escapeshellarg( $tmpfile ) ;
-	$out = array();
-	$ext = -1;
-	exec( $cmd , $out , $ext );
-
-	chdir( $old );
-
-	if ( $ext != 0 )
-	{
-		SvnUtil::CleanUp( $repo );
-		SvnUtil::Revert( $repo );
-
-		print "Falha de aplicação do patch [$ext]:\n\n";
-		foreach( $out as $o ) print "> $o\n";
-		print "\n";
-		die ( 1 );
-	}
-}
-unlink( $tmpfile );
-
-print "Status\n";
-
-$stat = SvnUtil::Status( $repo );
-if ( count( $stat->Files ) == 0 )
-{
-	print "No modified.";
-}
-else
-{
-	passthru( "/usr/bin/svn status " . escapeshellarg( $repo ) );
-}
-
-print "\n";
